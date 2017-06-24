c5968d1b765eea7d50d7ea9a398e41c824d6990d
Peter Thomas <peter_thomas@intuit.com>
2017-06-24 10:46:29 +0530

    workaround for the bad behavior of the net.masterthought.cucumber.ReportBuilder

--- karate-demo/src/test/java/demo/DemoTestParallel.java
+++ karate-demo/src/test/java/demo/DemoTestParallel.java
@@ -3,6 +3,13 @@ package demo;
 import com.intuit.karate.cucumber.CucumberRunner;
 import com.intuit.karate.cucumber.KarateStats;
 import cucumber.api.CucumberOptions;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -27,8 +34,21 @@ public class DemoTestParallel {
     
     @Test
     public void testParallel() {
-        KarateStats stats = CucumberRunner.parallel(getClass(), 5, "target/surefire-reports");
-        assertTrue("there are scenario failures", stats.getFailCount() == 0);
+        String karateOutputPath = "target/surefire-reports";
+        KarateStats stats = CucumberRunner.parallel(getClass(), 5, karateOutputPath);
+        generateReport(karateOutputPath);
+        assertTrue("there are scenario failures", stats.getFailCount() == 0);        
+    }
+    
+    private static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        for (File file : jsonFiles) {
+            jsonPaths.add(file.getAbsolutePath());
+        }
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();        
     }
     
 }
