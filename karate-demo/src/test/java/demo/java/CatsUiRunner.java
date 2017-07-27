8ea4ac7229aca94cac2c1a2f127bcb724f61845b
Peter Thomas <peter_thomas@intuit.com>
2017-07-27 08:23:14 +0530

    adding one more ui runner for java interop case

---
+++ karate-demo/src/test/java/demo/java/CatsUiRunner.java
@@ -0,0 +1,17 @@
+package demo.java;
+
+import com.intuit.karate.ui.App;
+import org.junit.Test;
+
+/**
+ *
+ * @author pthomas3
+ */
+public class CatsUiRunner {
+    
+    @Test
+    public void testApp() {
+        App.run("src/test/java/demo/java/cats-java.feature", "dev");
+    }    
+    
+}
