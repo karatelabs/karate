b90e6c6178a595d5e109f04dc9aabdceb4aa10fc
Tandra Sishtla, Sunil <Sunil_TandraSishtla@intuit.com>
2017-10-03 07:41:02 -0700

    Cleaned up pom.xml and added Javadoc

--- karate-core/src/main/java/com/intuit/karate/filter/TagFilter.java
+++ karate-core/src/main/java/com/intuit/karate/filter/TagFilter.java
@@ -3,9 +3,26 @@ package com.intuit.karate.filter;
 import cucumber.runtime.model.CucumberFeature;
 import cucumber.runtime.model.CucumberTagStatement;
 
+/**
+ * Interface that should be implemented to plug in
+ * any filtering based on cucumber tags
+ *
+ * @author ssishtla
+ */
+
 @FunctionalInterface
 public interface TagFilter {
 
+    /**
+     * Method to be over-ridden to evaluate if a feature/scenario
+     * should be skipped for execution
+     *
+     * @param feature
+     * @param cucumberTagStatement
+     * @return true - indicates feature should be skipped from
+     *         execution, otherwise false
+     * @throws TagFilterException - when thrown, stops
+     *         execution of all features
+     */
     boolean filter(CucumberFeature feature, CucumberTagStatement cucumberTagStatement) throws TagFilterException;
-
 }
