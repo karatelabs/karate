6c50bdc5fff7e6fb8aba62cad1f1a4abb10e680f
RavinderSinghMaan <rsmaan4u8@gmail.com>
2018-02-11 19:19:46 +0000

    #306 | Ravinder | Changes for review comment to remove concat.

--- karate-core/src/main/java/com/intuit/karate/ScriptValueMap.java
+++ karate-core/src/main/java/com/intuit/karate/ScriptValueMap.java
@@ -10,18 +10,18 @@ import java.util.Map;
 public class ScriptValueMap extends HashMap<String, ScriptValue> {
 
     public static final String VAR_RESPONSE = "response";
-    public static final String VAR_RESPONSE_COOKIES = VAR_RESPONSE+"Cookies";
-    public static final String VAR_RESPONSE_HEADERS = VAR_RESPONSE+"Headers";
-    public static final String VAR_RESPONSE_STATUS = VAR_RESPONSE+"Status";
-    public static final String VAR_RESPONSE_TIME = VAR_RESPONSE+"Time";
+    public static final String VAR_RESPONSE_COOKIES = "responseCookies";
+    public static final String VAR_RESPONSE_HEADERS = "responseHeaders";
+    public static final String VAR_RESPONSE_STATUS = "responseStatus";
+    public static final String VAR_RESPONSE_TIME = "responseTime";
 
     public static final String VAR_REQUEST = "request";
-    public static final String VAR_REQUEST_URL_BASE = VAR_REQUEST+"UrlBase";
-    public static final String VAR_REQUEST_URI = VAR_REQUEST+"Uri";
-    public static final String VAR_REQUEST_METHOD = VAR_REQUEST+"Method";
-    public static final String VAR_REQUEST_HEADERS = VAR_REQUEST+"Headers";
-    public static final String VAR_REQUEST_PARAMS = VAR_REQUEST+"Params";
-    public static final String VAR_REQUEST_BODY = VAR_REQUEST+"Body";
+    public static final String VAR_REQUEST_URL_BASE = "requestUrlBase";
+    public static final String VAR_REQUEST_URI = "requestUri";
+    public static final String VAR_REQUEST_METHOD = "requestMethod";
+    public static final String VAR_REQUEST_HEADERS = "requestHeaders";
+    public static final String VAR_REQUEST_PARAMS = "requestParams";
+    public static final String VAR_REQUEST_BODY = "requestBody";
 
     public ScriptValue put(String key, Object value) {
         ScriptValue sv = new ScriptValue(value);
