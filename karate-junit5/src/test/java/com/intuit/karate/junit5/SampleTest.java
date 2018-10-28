2577dafad439e2d53397a68846128697a5afea25
Peter Thomas <peter_thomas@intuit.com>
2018-10-28 11:52:22 +0530

    simplified junit5 example #569

--- karate-junit5/src/test/java/com/intuit/karate/junit5/SampleTest.java
+++ karate-junit5/src/test/java/com/intuit/karate/junit5/SampleTest.java
@@ -1,12 +1,8 @@
 package com.intuit.karate.junit5;
 
 import org.junit.jupiter.api.TestFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SampleTest {
-
-    private static final Logger logger = LoggerFactory.getLogger(SampleTest.class);
 
     @TestFactory
     public Object test() {
