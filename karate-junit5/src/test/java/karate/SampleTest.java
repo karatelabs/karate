7ac5ffd9ede57e34a98e4445dc1bfa52b5f9fa99
Christian Stein <sormuras@gmail.com>
2018-10-29 08:19:42 +0100

    Remove public modifier
    For details see https://junit.org/junit5/docs/current/user-guide/#writing-tests-classes-and-methods
    which states "Neither test classes nor test methods need to be public".

--- karate-junit5/src/test/java/karate/SampleTest.java
+++ karate-junit5/src/test/java/karate/SampleTest.java
@@ -3,20 +3,20 @@ package karate;
 import com.intuit.karate.junit5.Karate;
 import org.junit.jupiter.api.TestFactory;
 
-public class SampleTest {
+class SampleTest {
 
     @TestFactory
-    public Object testSample() {
+    Object testSample() {
         return Karate.feature("sample").relativeTo(getClass()).run();
     }
     
     @TestFactory
-    public Object testTags() {
+    Object testTags() {
         return Karate.feature("tags").tags("@second").relativeTo(getClass()).run();
     }
 
     @TestFactory
-    public Object testFullPath() {
+    Object testFullPath() {
         return Karate
                 .feature("classpath:karate/tags.feature")
                 .tags("@first").run();
