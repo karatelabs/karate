e07fbab8101d29e14ff54ffdd885835ab72b0ec7
Peter Thomas <peter_thomas@intuit.com>
2018-11-02 15:52:36 +0530

    fix npe introduced in #576

--- karate-core/src/main/java/com/intuit/karate/ui/AppSession2.java
+++ karate-core/src/main/java/com/intuit/karate/ui/AppSession2.java
@@ -85,7 +85,7 @@ public class AppSession2 {
     }
 
     public void setSelectedScenario(int index) {
-        if (index == -1 || index > scenarioPanels.size() || scenarioPanels.size() == 0) {
+        if (index == -1 || index > scenarioPanels.size() || scenarioPanels.isEmpty()) {
             return;
         }
         rootPane.setCenter(scenarioPanels.get(index));
