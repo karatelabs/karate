88157fab724e2c0d3b8a7c26bbe4b15bb429ab38
Marcus Östling <mpt.ostling@gmail.com>
2019-02-14 14:54:24 +0100

    feat #7 A new class for the adhoc coverage tool
    The class will hold public arrays for the tool to use.

---
+++ karate-core/src/main/java/com/intuit/karate/core/AdhocCoverageTool.java
@@ -0,0 +1,9 @@
+package com.intuit.karate.core;
+
+/**
+ * Class that hold public variable for the adhoc coverage tool.
+ */
+public class AdhocCoverageTool {
+    public static boolean[] ParseUrlPattern = new boolean[100];
+
+}
