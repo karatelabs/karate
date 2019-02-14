6d81bbcbc185e20510f6a98a7c2382ff63045887
Marcus Östling <mpt.ostling@gmail.com>
2019-02-14 15:06:11 +0100

    feat #7 improve AdhocCoverageTool class

--- karate-core/src/main/java/com/intuit/karate/core/AdhocCoverageTool.java
+++ karate-core/src/main/java/com/intuit/karate/core/AdhocCoverageTool.java
@@ -1,9 +1,14 @@
 package com.intuit.karate.core;
 
+import java.util.*;
+
 /**
  * Class that hold public variable for the adhoc coverage tool.
  */
 public class AdhocCoverageTool {
-    public static boolean[] ParseUrlPattern = new boolean[100];
-
+    public static HashMap<String, Boolean[]> m = new HashMap<>();
+    static {
+        // Initialize boolean arrays here:
+        // Ex: AdhocCoverageTool.m.put("ParseUriPattern", new Boolean[10]);
+    }
 }
