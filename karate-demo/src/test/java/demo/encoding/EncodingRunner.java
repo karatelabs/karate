3e85bead019dda62b5670734b0caba046a785c6e
Peter Thomas <peter_thomas@intuit.com>
2019-03-09 19:10:29 +0530

    fixed parallel runner to check for #686

--- karate-demo/src/test/java/demo/encoding/EncodingRunner.java
+++ karate-demo/src/test/java/demo/encoding/EncodingRunner.java
@@ -1,30 +1,11 @@
 package demo.encoding;
 
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import static demo.DemoTestParallel.generateReport;
 import demo.TestBase;
-import static org.junit.Assert.assertTrue;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 /**
  *
  * @author pthomas3
  */
-public class EncodingRunner {
-    
-    @BeforeClass
-    public static void beforeClass() throws Exception {        
-        TestBase.beforeClass();
-    }
-
-    @Test
-    public void testParallel() {
-        System.setProperty("karate.env", "demo"); // ensure reset if other tests (e.g. mock) had set env in CI
-        Results results = Runner.parallel(getClass(), 5);
-        generateReport(results.getReportDir());
-        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);        
-    }    
+public class EncodingRunner extends TestBase {
     
 }
