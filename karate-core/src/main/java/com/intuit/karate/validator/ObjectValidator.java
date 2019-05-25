e128bd65a2d0553a5628ba7f5b08a73f50374ee7
Peter Thomas <peter_thomas@intuit.com>
2019-05-25 10:27:21 +0530

    improve old code that can be optimized #787

--- karate-core/src/main/java/com/intuit/karate/validator/ObjectValidator.java
+++ karate-core/src/main/java/com/intuit/karate/validator/ObjectValidator.java
@@ -1,7 +1,6 @@
 package com.intuit.karate.validator;
 
 import com.intuit.karate.ScriptValue;
-import com.jayway.jsonpath.DocumentContext;
 
 /**
  *
@@ -11,19 +10,7 @@ public class ObjectValidator implements Validator {
 
     @Override
     public ValidationResult validate(ScriptValue value) {
-        switch (value.getType()) {
-            case JSON:
-                DocumentContext doc = value.getValue(DocumentContext.class);
-                if (!doc.jsonString().startsWith("{")) {
-                    return ValidationResult.fail("not a json object");
-                } else {
-                    return ValidationResult.PASS;
-                }
-            case MAP:
-                return ValidationResult.PASS;
-            default:
-                return ValidationResult.fail("not a json object");
-        }
+        return value.isMapLike() ? ValidationResult.PASS : ValidationResult.fail("not a json object");
     }
 
     public static final ObjectValidator INSTANCE = new ObjectValidator();
