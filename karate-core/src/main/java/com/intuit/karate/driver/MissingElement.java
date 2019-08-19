bc3dbcca0d18190d1d0a5b4909dc6d3089776ed2
Peter Thomas <peter_thomas@intuit.com>
2019-08-18 19:56:08 -0700

    adjust maven profile for ui in hope that it works

--- karate-core/src/main/java/com/intuit/karate/driver/MissingElement.java
+++ karate-core/src/main/java/com/intuit/karate/driver/MissingElement.java
@@ -73,7 +73,7 @@ public class MissingElement implements Element {
     }        
 
     @Override
-    public DriverMouse mouse() {
+    public Mouse mouse() {
         return null;
     }        
 
