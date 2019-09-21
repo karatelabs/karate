fdbc8a2cc1deb1e03b22c157144db90c155aaadf
Peter Thomas <peter_thomas@intuit.com>
2019-09-20 21:20:17 -0700

    for sake of debugger, driver element to be stringify-able

--- karate-core/src/test/java/com/intuit/karate/driver/DriverElementTest.java
+++ karate-core/src/test/java/com/intuit/karate/driver/DriverElementTest.java
@@ -1,5 +1,6 @@
 package com.intuit.karate.driver;
 
+import com.intuit.karate.JsonUtils;
 import com.intuit.karate.ScriptValue;
 import java.util.Collections;
 import java.util.List;
@@ -10,14 +11,14 @@ import org.junit.Test;
  * @author pthomas3
  */
 public class DriverElementTest {
-    
+
     @Test
     public void testToJson() {
+        JsonUtils.toJsonDoc("{}");
         Element de = DriverElement.locatorExists(null, "foo");
         List list = Collections.singletonList(de);
         ScriptValue sv = new ScriptValue(list);
-        // TODO fix this
-        // sv.getAsString();
+        sv.getAsString();
     }
-    
+
 }
