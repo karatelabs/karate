8107885618df78a217cfb8d4fc281658121e41d5
Manoj Y <manoj_yadav@intuit.com>
2019-10-20 11:07:15 +0530

    Updated karate-demo to spring boot 2
    Updated karate-demo to spring boot 2

--- karate-demo/src/test/java/ssl/TestService.java
+++ karate-demo/src/test/java/ssl/TestService.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
