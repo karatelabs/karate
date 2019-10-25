68d3cfc9409ea4ac2a9d24ad4038ece4acb0e017
BadgerOps <github@badgerops.net>
2019-10-25 11:02:06 -0700

    example of multistage docker build to add chrome version as a label

--- karate-docker/karate-chrome/build.sh
+++ karate-docker/karate-chrome/build.sh
@@ -3,4 +3,8 @@ set -x -e
 cd ../..
 docker run -it --rm -v "$(pwd)":/karate -w /karate -v "$(pwd)"/karate-docker/karate-chrome/target:/root/.m2 maven:3-jdk-8 bash karate-docker/karate-chrome/install.sh
 cd karate-docker/karate-chrome
-docker build -t karate-chrome .
+docker build -t karate-chrome:build . -f Dockerfile.build
+
+CHROME_VERSION=`docker run --rm -it karate-chrome:build /usr/bin/google-chrome --version | cut -d' ' -f3`
+
+docker build --build-arg chrome_version=${CHROME_VERSION} -t karate-chrome:latest . -f Dockerfile
