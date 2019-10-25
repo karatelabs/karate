b212736bbfe9a668d01946a23d99fbc94268e9f7
Peter Thomas <peter_thomas@intuit.com>
2019-10-25 10:19:09 +0530

    minor code cleanup

--- karate-core/src/main/java/com/intuit/karate/ScriptBindings.java
+++ karate-core/src/main/java/com/intuit/karate/ScriptBindings.java
@@ -142,7 +142,8 @@ public class ScriptBindings implements Bindings {
         } catch (KarateAbortException | KarateFileNotFoundException ke) {
             throw ke; // reduce log bloat for common file-not-found situation / handle karate.abort()
         } catch (Exception e) {
-            throw new RuntimeException("javascript evaluation failed: " + exp + ", " + e.getMessage(), e);
+            String append = e.getMessage() == null ? exp : e.getMessage();
+            throw new RuntimeException("javascript evaluation failed: " + exp + ", " + append, e);
         }
     }
 
