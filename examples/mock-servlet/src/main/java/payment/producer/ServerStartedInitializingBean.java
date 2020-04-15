dfdbfd91e323ff2926ee104b4403883c52ca11a7
Peter Thomas <ptrthomas@gmail.com>
2020-04-15 12:39:22 +0530

    identified solution for mock-servlet + spring boot 2 ref #1097 just change the custom mock servlet code slightly, also recommend error handling

--- examples/mock-servlet/src/main/java/payment/producer/ServerStartedInitializingBean.java
+++ examples/mock-servlet/src/main/java/payment/producer/ServerStartedInitializingBean.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
  * @author pthomas3
  */
 @Component
-public class ServerStartedInitializingBean implements ApplicationRunner, ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+public class ServerStartedInitializingBean implements ApplicationRunner, ApplicationListener<WebServerInitializedEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(ServerStartedInitializingBean.class);
 
@@ -30,8 +30,8 @@ public class ServerStartedInitializingBean implements ApplicationRunner, Applica
     }
 
     @Override
-    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent e) {
-        localPort = e.getEmbeddedServletContainer().getPort();
+    public void onApplicationEvent(WebServerInitializedEvent e) {
+        localPort = e.getWebServer().getPort();
         logger.info("after runtime init, local server port: {}", localPort);
     }
 
--- examples/mock-servlet/src/test/java/payment/mock/servlet/MockSpringMvcServlet.java
+++ examples/mock-servlet/src/test/java/payment/mock/servlet/MockSpringMvcServlet.java
@@ -25,14 +25,11 @@ package payment.mock.servlet;
 
 import com.intuit.karate.http.HttpRequestBuilder;
 import com.intuit.karate.mock.servlet.MockHttpClient;
-
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.springframework.boot.autoconfigure.web.WebMvcProperties;
 import org.springframework.mock.web.MockServletConfig;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -43,7 +40,7 @@ import org.springframework.web.servlet.DispatcherServlet;
  * @author pthomas3
  */
 public class MockSpringMvcServlet extends MockHttpClient {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(MockSpringMvcServlet.class);
 
     private final Servlet servlet;
@@ -79,30 +76,11 @@ public class MockSpringMvcServlet extends MockHttpClient {
         ServletConfig servletConfig = new MockServletConfig();
         try {
             servlet.init(servletConfig);
-            customize(servlet);
         } catch (Exception e) {
             logger.error("init failed: {}", e.getMessage());
             throw new RuntimeException(e);
         }
         return servlet;
-    }
-
-    /**
-     * Checks if servlet is Dispatcher servlet implementation and then fetches
-     * the WebMvcProperties from spring container and configure the dispatcher
-     * servlet.
-     *
-     * @param servlet input servlet implementation
-     */
-    private static void customize(Servlet servlet) {
-        if (servlet instanceof DispatcherServlet) {
-            DispatcherServlet dispatcherServlet = (DispatcherServlet) servlet;
-            WebMvcProperties mvcProperties
-                    = dispatcherServlet.getWebApplicationContext().getBean(WebMvcProperties.class);
-            dispatcherServlet.setThrowExceptionIfNoHandlerFound(mvcProperties.isThrowExceptionIfNoHandlerFound());
-            dispatcherServlet.setDispatchOptionsRequest(mvcProperties.isDispatchOptionsRequest());
-            dispatcherServlet.setDispatchTraceRequest(mvcProperties.isDispatchTraceRequest());
-        }
     }
 
     public static MockSpringMvcServlet getMock() {
