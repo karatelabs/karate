1af1b86ff7eea743ec717c1b3013366385036321
Peter Thomas <ptrthomas@gmail.com>
2020-04-15 13:08:26 +0530

    clean up of code for prev 2 commits

--- examples/mock-servlet/src/test/java/payment/mock/servlet/PaymentMockTest.java
+++ examples/mock-servlet/src/test/java/payment/mock/servlet/PaymentMockTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package payment.mock.servlet;
 
 import com.intuit.karate.Results;
@@ -20,10 +15,7 @@ public class PaymentMockTest {
     
     @Test
     public void testMock() {
-        System.setProperty("karate.env", "mock");
-//        ConfigurableApplicationContext context = PaymentService.start();
-//        String paymentServiceUrl = "http://localhost:" + PaymentService.getPort(context);
-//        System.setProperty("payment.service.url", paymentServiceUrl);        
+        System.setProperty("karate.env", "mock");        
         Results results = Runner.path("classpath:payment/producer").parallel(1);
         assertTrue("there are scenario failures", results.getFailCount() == 0);
     }
