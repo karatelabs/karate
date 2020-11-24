84b37558765d3e650774ee9dfa68c3f81bef6931
Matthew Houlden <>
2020-11-24 12:11:43 +0000

    add test cases for PerfHookTest.java

--- karate-core/src/test/java/com/intuit/karate/core/PerfHookTest.java
+++ karate-core/src/test/java/com/intuit/karate/core/PerfHookTest.java
@@ -4,10 +4,16 @@ import com.intuit.karate.PerfHook;
 import com.intuit.karate.Runner;
 import com.intuit.karate.http.HttpRequest;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+
+import com.intuit.karate.match.Match;
+import com.intuit.karate.match.MatchResult;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,19 +36,84 @@ class PerfHookTest {
         System.setProperty("karate.server.port", server.getPort() + "");
     }
 
+    @BeforeEach
+    void beforeEach() {
+        eventName = null;
+        failed = null;
+        vars = null;
+    }
+
     @AfterAll
     static void afterAll() {
         server.stop();
     }
 
     @Test
-    void testPerfHook() {
-        Map<String, Object> arg = Collections.singletonMap("foo", "bar");
-        Runner.callAsync("classpath:com/intuit/karate/core/perf.feature", Collections.EMPTY_LIST, arg, perfHook);
-        assertEquals(eventName, "http://localhost:" + server.getPort() + "/hello?foo=bar");
+    void testPerfHook1() {
+        // Run a passing scenario
+        List<String> tags = Collections.singletonList("@name=pass");
+        String bar = "one";
+        Map<String, Object> arg = Collections.singletonMap("bar", bar);
+        Runner.callAsync("classpath:com/intuit/karate/core/perf.feature", tags, arg, perfHook);
+        assertEquals(eventName, "http://localhost:" + server.getPort() + "/hello?foo=" + bar);
+        assertFalse(failed);
+        match(vars, "{ bar: '" + bar + "', responseHeaders: { content-type: ['application/json'], content-length: [#string], server: [#string], date: [#string] }, configSource: 'normal', responseStatus: 200, response: { foo: ['" + bar + "'] } }");
+    }
+
+    @Test
+    void testPerfHook2() {
+        // Run a scenario which fails the status check
+        List<String> tags = Collections.singletonList("@name=failStatus");
+        String bar = "two";
+        Map<String, Object> arg = Collections.singletonMap("bar", bar);
+        Runner.callAsync("classpath:com/intuit/karate/core/perf.feature", tags, arg, perfHook);
+        assertEquals(eventName, "http://localhost:" + server.getPort() + "/hello?foo=" + bar);
+        assertTrue(failed);
+        match(vars, "{ bar: '" + bar + "', responseHeaders: { content-type: ['application/json'], content-length: [#string], server: [#string], date: [#string] }, configSource: 'normal', responseStatus: 200, response: { foo: ['" + bar + "'] } }");
+    }
+
+    @Test
+    void testPerfHook3() {
+        // Run a scenario which fails the response match
+        List<String> tags = Collections.singletonList("@name=failResponse");
+        String bar = "three";
+        Map<String, Object> arg = Collections.singletonMap("bar", bar);
+        Runner.callAsync("classpath:com/intuit/karate/core/perf.feature", tags, arg, perfHook);
+        assertEquals(eventName, "http://localhost:" + server.getPort() + "/hello?foo=" + bar);
+        assertTrue(failed);
+        match(vars, "{ bar: '" + bar + "', responseHeaders: { content-type: ['application/json'], content-length: [#string], server: [#string], date: [#string] }, configSource: 'normal', responseStatus: 200, response: { foo: ['" + bar + "'] } }");
+    }
+
+    @Test
+    void testPerfHook4() {
+        // Run a scenario without passing a required argument
+        List<String> tags = Collections.singletonList("@name=pass");
+        Map<String, Object> arg = Collections.emptyMap();
+        Runner.callAsync("classpath:com/intuit/karate/core/perf.feature", tags, arg, perfHook);
+        assertNull(eventName);
+        assertTrue(failed);
+        match(vars, "{ configSource: 'normal' }");
+    }
+
+    @Test
+    void testPerfHook5() {
+        // Run a scenario which doesn't exist
+        List<String> tags = Collections.singletonList("xyz");
+        Map<String, Object> arg = Collections.emptyMap();
+        Runner.callAsync("classpath:com/intuit/karate/core/perf.feature", tags, arg, perfHook);
+        assertNull(eventName);
+        assertFalse(failed);
+        assertNull(vars);
+    }
+
+    private void match(Object actual, Object expected) {
+        MatchResult mr = Match.that(actual).isEqualTo(expected);
+        assertTrue(mr.pass, mr.message);
     }
     
     String eventName;
+    Boolean failed;
+    Map<String, Object> vars;
 
     PerfHook perfHook = new PerfHook() {
 
@@ -66,6 +137,8 @@ class PerfHookTest {
         @Override
         public void afterFeature(boolean failed, Map<String, Object> vars) {
             logger.debug("afterFeature called");
+            PerfHookTest.this.failed = failed;
+            PerfHookTest.this.vars = vars;
         }
     };
 
