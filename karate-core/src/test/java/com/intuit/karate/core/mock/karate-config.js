f9136c499c34193a1d77f866b5dbb51a304b7296
Peter Thomas <ptrthomas@gmail.com>
2021-01-30 13:38:04 +0530

    add ssl test to core to see if maven shade is the culprit

--- karate-core/src/test/java/com/intuit/karate/core/mock/karate-config.js
+++ karate-core/src/test/java/com/intuit/karate/core/mock/karate-config.js
@@ -1,6 +1,10 @@
 function fn() {
   var port = karate.properties['karate.server.port'] || 8080;
+  var prefix = karate.properties['karate.ssl'] ? 'https' : 'http';
+  if (prefix === 'https') {
+    karate.configure('ssl', true);
+  }
   return {
-    mockServerUrl: 'http://localhost:' + port + '/v1/'
+    mockServerUrl: prefix + '://localhost:' + port + '/v1/'
   }
 }
