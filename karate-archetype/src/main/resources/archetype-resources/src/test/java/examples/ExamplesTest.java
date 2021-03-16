0720b1552235c0946a6144cb84dde8d33164c866
Peter Thomas <ptrthomas@gmail.com>
2021-03-16 14:46:17 +0530

    improving archetype with parallel runner example

--- karate-archetype/src/main/resources/archetype-resources/src/test/java/examples/ExamplesTest.java
+++ karate-archetype/src/main/resources/archetype-resources/src/test/java/examples/ExamplesTest.java
@@ -1,14 +1,19 @@
 package examples;
 
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class ExamplesTest {
-    
-    // this will run all *.feature files that exist in sub-directories
-    // see https://github.com/intuit/karate#naming-conventions   
-    @Karate.Test
-    Karate testAll() {
-        return Karate.run().relativeTo(getClass());
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:examples")
+                .tags("~@ignore")
+                //.outputCucumberJson(true)
+                .parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
-    
+
 }
