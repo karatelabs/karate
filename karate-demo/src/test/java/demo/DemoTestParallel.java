dd419bbf9f5d778236c23f05087d8f79292b97db
Baur Urazalinov <burazalinov@signifyhealth.com>
2021-05-13 10:27:02 -0500

    Corrected. Got feedback.

--- karate-demo/src/test/java/demo/DemoTestParallel.java
+++ karate-demo/src/test/java/demo/DemoTestParallel.java
@@ -38,7 +38,7 @@ public class DemoTestParallel {
 
     public static void generateReport(String karateOutputPath) {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
-        List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
         Configuration config = new Configuration(new File("target"), "demo");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
