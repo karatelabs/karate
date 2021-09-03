f642b8308054f0b6bb62246f99dbad279d7d71aa
Peter Thomas <ptrthomas@gmail.com>
2021-09-03 13:18:45 +0530

    support htmx form enctype for multipart

--- karate-core/src/main/java/com/intuit/karate/template/KarateServerDialect.java
+++ karate-core/src/main/java/com/intuit/karate/template/KarateServerDialect.java
@@ -52,6 +52,7 @@ public class KarateServerDialect extends AbstractProcessorDialect {
         ps.add(new KaHxAnyAttrProcessor(dialectPrefix, "target"));
         ps.add(new KaHxAnyAttrProcessor(dialectPrefix, "swap"));
         ps.add(new KaHxAnyAttrProcessor(dialectPrefix, "trigger"));
+        ps.add(new KaHxAnyAttrProcessor(dialectPrefix, "encoding"));
         ps.add(new KaHxPathAttrProcessor(dialectPrefix, "get", config));
         ps.add(new KaHxPathAttrProcessor(dialectPrefix, "post", config));
         ps.add(new KaHxPathAttrProcessor(dialectPrefix, "put", config));
