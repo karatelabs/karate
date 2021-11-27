5a55689574a38dfd0262df5affc6b75700b8d41f
Joel Ramos <joelramos@deloitte.ca>
2021-11-27 12:12:20 -0500

    actually attempting to increase timeout

--- karate-e2e-tests/src/test/java/driver/karate-config-docker.js
+++ karate-e2e-tests/src/test/java/driver/karate-config-docker.js
@@ -3,6 +3,7 @@ function fn() {
     type: 'chrome',
     showDriverLog: true,
     start: false,
+    timeout: 60000,
     beforeStart: 'supervisorctl start ffmpeg',
     afterStop: 'supervisorctl stop ffmpeg',
     videoFile: '/tmp/karate.mp4'
@@ -11,7 +12,6 @@ function fn() {
   var serverPort = karate.properties['server.port'] || 8080;
   var config = {
         driverType: 'chrome',
-        timeout: 60000,
         serverUrl: 'http://' + hostname + ':' + serverPort
     };
   return config;
