ba79a6e83ed186479db0cfb9c32e83302333b1b7
Joel Ramos <joelramos@deloitte.ca>
2021-11-27 07:53:44 -0500

    adding support for runs in Windows (docker for desktop accessing host)

--- karate-e2e-tests/src/test/java/driver/karate-config.js
+++ karate-e2e-tests/src/test/java/driver/karate-config.js
@@ -1,7 +1,8 @@
 function fn() {
   var serverPort = karate.properties['server.port'] || 8080;
+  var hostname = com.intuit.karate.FileUtils.isOsWindows() ? 'host.docker.internal' : 'localhost'
   var config = {
-    serverUrl: 'http://localhost:' + serverPort
+    serverUrl: 'http://' + hostname + ':' + serverPort
   };
   return config;
 }
