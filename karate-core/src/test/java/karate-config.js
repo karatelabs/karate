bf3ac0b487acdc6e25f774dad073263fd59429c1
BEAST-PC\dines <dinesh19aug@gmail.com>
2022-03-26 04:57:33 -0400

    Issue #1837 - Removed the sample test case based on  @ptrthomas comments.

--- karate-core/src/test/java/karate-config.js
+++ karate-core/src/test/java/karate-config.js
@@ -1,15 +1,3 @@
 function fn() {
-  console.log("HEllo");
-  var env = karate.env; // get system property 'karate.env'
-  var config = {
-    env: env,
-    secret: 'secretvalueshouldnotcomeincucumberreport',
-    baseURL: 'https://jsonplaceholder.typicode.com',
-    number: '2'
-  }
-  //var results = karate.callSingle('classpath:com/intuit/karate/report/suite.feature@test1',config);
-  //results = karate.callSingle('classpath:com/intuit/karate/report/suite2.feature@test2',config);
-  return {
-
-    configSource: 'normal' }
+  return { configSource: 'normal' }
 }
