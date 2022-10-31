3c4c1ea3444e745451c125c97b9393bb9c7062b2
Peter Thomas <ptrthomas@gmail.com>
2022-10-31 17:12:02 +0530

    update code ref #2153

--- karate-core/src/main/java/com/intuit/karate/ImageComparison.java
+++ karate-core/src/main/java/com/intuit/karate/ImageComparison.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2022 Karate Labs Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.intuit.karate;
 
 import io.github.t12y.resemble.Resemble;
@@ -19,6 +42,7 @@ import java.util.*;
 import static io.github.t12y.ssim.SSIM.ssim;
 
 public class ImageComparison {
+
     public static final String RESEMBLE = "resemble";
     public static final String SSIM = "ssim";
 
@@ -128,7 +152,7 @@ public class ImageComparison {
     }
 
     public static Map<String, Object> compare(byte[] baselineImg, byte[] latestImg, Map<String, Object> options,
-                                              Map<String, Object> defaultOptions) throws MismatchException {
+            Map<String, Object> defaultOptions) throws MismatchException {
 
         boolean allowScaling = toBool(defaultOptions.get("allowScaling"));
         ImageComparison imageComparison = new ImageComparison(baselineImg, latestImg, options, allowScaling);
@@ -164,7 +188,9 @@ public class ImageComparison {
                 mismatchPercentage = currentMismatchPercentage;
             }
 
-            if (mismatchPercentage < imageComparison.stopWhenMismatchIsLessThan) break;
+            if (mismatchPercentage < imageComparison.stopWhenMismatchIsLessThan) {
+                break;
+            }
         }
 
         return imageComparison.checkMismatch(mismatchPercentage);
@@ -173,10 +199,12 @@ public class ImageComparison {
     private Map<String, Object> checkMismatch(double mismatchPercentage) {
         result.put("mismatchPercentage", mismatchPercentage);
 
-        if (mismatchPercentage <= 0.0 || mismatchPercentage < failureThreshold) return result;
+        if (mismatchPercentage <= 0.0 || mismatchPercentage < failureThreshold) {
+            return result;
+        }
 
-        String msg = "latest image differed from baseline more than allowable threshold: " +
-                mismatchPercentage + "% >= " + failureThreshold + "%";
+        String msg = "latest image differed from baseline more than allowable threshold: "
+                + mismatchPercentage + "% >= " + failureThreshold + "%";
 
         result.put("isMismatch", true);
 
@@ -237,7 +265,7 @@ public class ImageComparison {
 
         Object tolerancesObj = options.get("tolerances");
         if (tolerancesObj instanceof Map) {
-            Map tolerances = (Map)tolerancesObj;
+            Map tolerances = (Map) tolerancesObj;
             opts.redTolerance = toDouble(tolerances.get("red"), opts.redTolerance);
             opts.greenTolerance = toDouble(tolerances.get("green"), opts.greenTolerance);
             opts.blueTolerance = toDouble(tolerances.get("blue"), opts.blueTolerance);
@@ -268,12 +296,16 @@ public class ImageComparison {
     }
 
     private boolean getBool(String name, boolean defaultValue) {
-        if (!options.containsKey(name)) return defaultValue;
+        if (!options.containsKey(name)) {
+            return defaultValue;
+        }
         return toBool(options.get(name));
     }
 
     private static boolean toBool(Object obj) {
-        if (obj == null) return false;
+        if (obj == null) {
+            return false;
+        }
         return Boolean.parseBoolean(asString(obj));
     }
 
@@ -282,46 +314,60 @@ public class ImageComparison {
     }
 
     private static double toDouble(Object obj, double defaultValue) {
-        if (!(obj instanceof Number)) return defaultValue;
-        return ((Number)obj).doubleValue();
+        if (!(obj instanceof Number)) {
+            return defaultValue;
+        }
+        return ((Number) obj).doubleValue();
     }
 
     private int getInt(String name, int defaultValue) {
         Object val = options.get(name);
-        if (!(val instanceof Number)) return defaultValue;
-        return ((Number)val).intValue();
+        if (!(val instanceof Number)) {
+            return defaultValue;
+        }
+        return ((Number) val).intValue();
     }
 
     private String getString(String name, String defaultValue) {
-        if (!options.containsKey(name)) return defaultValue;
+        if (!options.containsKey(name)) {
+            return defaultValue;
+        }
         return asString(options.get(name));
     }
 
     private static String asString(Object obj) {
-        if (obj == null) return null;
+        if (obj == null) {
+            return null;
+        }
         return obj.toString();
     }
 
     private int[][] getIgnoredBoxes() {
         Object boxes = options.get("ignoredBoxes");
-        if (!(boxes instanceof Collection)) return null;
+        if (!(boxes instanceof Collection)) {
+            return null;
+        }
 
         List<int[]> ignoredBoxes = new ArrayList<>();
-        for(Object boxObj : (Collection)boxes) {
+        for (Object boxObj : (Collection) boxes) {
             int[] ignoredBox = getIntArray(boxObj, IGNORED_BOX_KEYS);
-            if (ignoredBox != null) ignoredBoxes.add(ignoredBox);
+            if (ignoredBox != null) {
+                ignoredBoxes.add(ignoredBox);
+            }
         }
 
         return ignoredBoxes.toArray(new int[0][]);
     }
 
     private static int[] getIntArray(Object obj, String[] keys) {
-        if (!(obj instanceof Map)) return null;
+        if (!(obj instanceof Map)) {
+            return null;
+        }
 
         int[] vals = new int[keys.length];
-        Map m = (Map)obj;
+        Map m = (Map) obj;
 
-        for(int i=0; i<keys.length; i++) {
+        for (int i = 0; i < keys.length; i++) {
             Object val = m.get(keys[i]);
             if (val instanceof Number) {
                 vals[i] = ((Number) val).intValue();
@@ -343,7 +389,7 @@ public class ImageComparison {
             packedPixel = packed[i];
             unpackedIndex = i * 4;
 
-            unpacked[unpackedIndex    ] = (0xff & (packedPixel >> 16));
+            unpacked[unpackedIndex] = (0xff & (packedPixel >> 16));
             unpacked[unpackedIndex + 1] = (0xff & (packedPixel >> 8));
             unpacked[unpackedIndex + 2] = (0xff & packedPixel);
             unpacked[unpackedIndex + 3] = (0xff & (packedPixel >>> 24));
@@ -353,11 +399,14 @@ public class ImageComparison {
     }
 
     public static class MismatchException extends RuntimeException {
+
         public Map<String, Object> data;
+
         public MismatchException(String msg, Map<String, Object> data) {
             super(msg);
             data.put("error", getMessage());
             this.data = data;
         }
     }
+    
 }
