33800a1c2294a4ef35e30eee978a53e8414271ba
Peter Thomas <ptrthomas@gmail.com>
2022-12-14 10:14:19 +0530

    edit initial block comment

--- karate-core/src/main/java/com/intuit/karate/template/KaHxAnyAttrProcessor.java
+++ karate-core/src/main/java/com/intuit/karate/template/KaHxAnyAttrProcessor.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2020 Intuit Inc.
+ * Copyright 2022 Karate Labs Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
