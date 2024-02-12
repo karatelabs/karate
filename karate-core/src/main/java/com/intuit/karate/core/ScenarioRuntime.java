b14076e6093fae355ef01582f9d2e668b6641d4d
Aditya Kishore <akushwarrior@gmail.com>
2024-02-12 17:45:49 -0500

    controlled reversion in ScenarioRuntime that allows for self calls and preserves ability to run single scenarios out of features

--- karate-core/src/main/java/com/intuit/karate/core/ScenarioRuntime.java
+++ karate-core/src/main/java/com/intuit/karate/core/ScenarioRuntime.java
@@ -331,14 +331,15 @@ public class ScenarioRuntime implements Runnable {
             return false;
         }
         String callTag = fr.featureCall.callTag;
-        if (callTag != null && (!fr.caller.isNone() || fr.perfHook != null)) {
+        if (callTag != null) {
             // only if this is a legit "call" or a gatling "call by tag"
-            if (tags.contains(callTag)) {
+            if (tags.contains(callTag) && (!fr.caller.isNone() || fr.perfHook != null)) {
                 logger.info("{} - call by tag at line {}: {}", fr, scenario.getLine(), callTag);
                 return true;
+            } else if (!fr.caller.isNone() || fr.perfHook != null) {
+                logger.trace("skipping scenario at line: {} with call by tag effective: {}", scenario.getLine(), callTag);
+                return false;
             }
-            logger.trace("skipping scenario at line: {} with call by tag effective: {}", scenario.getLine(), callTag);
-            return false;
         }
         if (fr.caller.isNone()) {
             if (tags.evaluate(fr.suite.tagSelector, fr.suite.env)) {
