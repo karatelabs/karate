82a3bdaf2af75f7d2fae0473405f03298e0057a4
Peter Thomas <ptrthomas@gmail.com>
2025-01-03 13:59:24 +0530

    add a few more for #2633

--- karate-core/src/main/java/com/intuit/karate/Runner.java
+++ karate-core/src/main/java/com/intuit/karate/Runner.java
@@ -173,6 +173,15 @@ public class Runner {
             if (tempOptions != null) {
                 LOGGER.info("using system property '{}': {}", Constants.KARATE_OPTIONS, tempOptions);
                 Main ko = Main.parseKarateOptions(tempOptions);
+                if (ko.name != null) {
+                    scenarioName = ko.name;
+                }
+                if (ko.configDir != null) {
+                    configDir = ko.configDir;
+                }
+                if (ko.env != null) {
+                    env = ko.env;
+                }
                 if (ko.reportDir != null) {
                     reportDir = ko.reportDir;
                 }
